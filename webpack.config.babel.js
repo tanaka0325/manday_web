cddcd965883666e845e4d02b3340dd76189602d2
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-01-05 22:23:42 +0900

    Build sass and html by webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,30 @@
 import path from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 module.exports = [
   {
+    name: 'html',
+    entry: {
+      html: path.join(__dirname, 'src', 'index.html'),
+    },
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'index.html',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.html$/,
+          loader: ExtractTextPlugin.extract('raw'),
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('index.html'),
+    ],
+  },
+  {
+    name: 'js',
     entry: {
       bundle: path.join(__dirname, 'src', 'js', 'index.js'),
     },
@@ -19,5 +42,25 @@ module.exports = [
         },
       ],
     },
+  },
+  {
+    name: 'style',
+    entry: path.join(__dirname, 'src', 'sass', 'style.sass'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'style.css',
+    },
+    target: 'web',
+    module: {
+      loaders: [
+        {
+          test: /\.sass$/,
+          loader: ExtractTextPlugin.extract('css?minimize!sass'),
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('style.css'),
+    ],
   },
 ]
