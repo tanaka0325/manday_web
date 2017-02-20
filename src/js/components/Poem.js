17c8aa1a939ca312c5f9eddee7f9027202964e22
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-02-21 02:06:10 +0900

    Add date in poem

--- src/js/components/Poem.js
+++ src/js/components/Poem.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import MarkdownIt from 'markdown-it'
+import moment from 'moment'
 
 const Poem = (props) => {
   const md = new MarkdownIt()
   const poemHtml = md.render(props.poem.body)
+  console.log(moment(props.poem.created_at).format('HH:mm:ss'))
 
   return (
     <article className="message poem">
       <div className="message-body">
         <p dangerouslySetInnerHTML={{ __html: poemHtml }} />
+        {/* <small>{moment(props.poem.created_at).format('HH:mm:ss')}</small> */}
+        <small className="time">{moment(props.poem.created_at).fromNow()}</small>
+        <br/>
         <button onClick={() => { props.actions.delete(props.poem) }}>delete</button>
       </div>
     </article>
